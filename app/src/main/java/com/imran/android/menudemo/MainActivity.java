33b50d50c78facf56efc437889ba4439d6f9b34f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-13 01:25:03 +0600

    menu

--- app/src/main/java/com/imran/android/menudemo/MainActivity.java
+++ app/src/main/java/com/imran/android/menudemo/MainActivity.java
@@ -3,8 +3,18 @@ package com.imran.android.menudemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
