492fcdb2a80a37a9f81cf7709183fbdd396be700
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-13 01:34:37 +0600

    menu

--- app/src/main/java/com/imran/android/menudemo/MainActivity.java
+++ app/src/main/java/com/imran/android/menudemo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.imran.android.menudemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.time.Duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.main_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this,"Setting", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.help:
+                Toast.makeText(this, "Help", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return  false;
+        }
     }
 
     @Override
