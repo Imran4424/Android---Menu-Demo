9d2ffb7eb0ead76958f336445d99665f19f2bc63
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-03-12 00:29:51 +0600

    updated

--- app/src/main/java/com/imran/android/menudemo/MainActivity.java
+++ app/src/main/java/com/imran/android/menudemo/MainActivity.java
@@ -14,6 +14,12 @@ import java.time.Duration;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
@@ -37,11 +43,5 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return  false;
         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
